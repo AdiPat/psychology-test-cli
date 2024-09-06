40d75a89a9908ade0ab711371c1fdccc7a169fff
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-07 05:25:44 +0530

    setup basic interface

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,47 @@
-async function run() {
-  console.log("Hello, World!");
+import { Command } from "commander";
+
+function run() {
+  const program = new Command();
+
+  program
+    .option("--generate <type>", "Generate a test")
+    .option("--run", "Run a generated test.")
+    .option("--name <name>", "Test name")
+    .option("--description <description>", "Test description")
+    .helpOption("-h, --help", "Display help for command")
+    .parse(process.argv);
+
+  const { name, description, generate, help, run } = program.opts();
+
+  if (!run && !generate && !help) {
+    console.error("Error: Please provide a command to run.");
+    program.help();
+    process.exit(1);
+  }
+
+  if (!name) {
+    console.error("Error: Test name is required");
+    process.exit(1);
+  }
+
+  if (!description) {
+    console.error("Error: Test description is required");
+    process.exit(1);
+  }
+
+  if (!generate) {
+    console.error("Error: Test type is required");
+    process.exit(1);
+  }
+
+  if (help) {
+    program.help();
+    process.exit(0);
+  }
+
+  console.log("Name:", name);
+  console.log("Description:", description);
+  console.log("Type:", generate);
 }
 
 export { run };
